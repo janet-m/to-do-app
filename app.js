2c214250b80e508a38bdc864c747c3ede43ca6b3
Janet Miller <janet@janets-mbp-2.lan>
2017-09-21 09:40:51 -0700

    checkpoint-7-events-1 completed

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ function onReady() {
     let newLi = document.createElement('li');
     let checkbox = document.createElement('input');
     checkbox.type = "checkbox";
+    let delButton = document.createElement('button');
+    let delLabel = document.createTextNode("Delete");
+    delButton.appendChild(delLabel);
     newLi.textContent = title;
     toDoList.appendChild(newLi);
     newLi.appendChild(checkbox);
+    newLi.appendChild(delButton);
     newToDoText.value = '';
+    delButton.addEventListener("click", (event) => {
+      toDoList.removeChild(newLi);
+    });
+
   });
 }
 
