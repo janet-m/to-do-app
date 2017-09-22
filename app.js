c46de49bf726bc83479cd023d20f7a08932c3327
Janet Miller <janet@janets-mbp-2.lan>
2017-09-21 22:08:53 -0700

    checkpoint-8-events-2: completed except for 'delete' button

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList  = document.getElementById('toDoList');
-
-  addToDoForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let title = newToDoText.value;
-    let newLi = document.createElement('li');
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    let delButton = document.createElement('button');
-    let delLabel = document.createTextNode("Delete");
-    delButton.appendChild(delLabel);
-    newLi.textContent = title;
-    toDoList.appendChild(newLi);
-    newLi.appendChild(checkbox);
-    newLi.appendChild(delButton);
-    newToDoText.value = '';
-    delButton.addEventListener("click", (event) => {
-      toDoList.removeChild(newLi);
-    });
-
-  });
-}
-
 window.onload = function() {
   onReady();
 };
+
+function onReady() {
+  var toDos = [];
+  var addToDoForm = document.getElementById('addToDoForm');
+
+  addToDoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  function createNewToDo() {
+    var newToDoText = document.getElementById('newToDoText');
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = '';
+
+    renderTheUI(toDos);
+  }
+
+  function renderTheUI(toDos) {
+    var toDoList = document.getElementById('toDoList');
+
+    toDoList.innerHTML = ''
+
+    toDos.forEach(function(toDo) {
+      var newLi = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.innerHTML = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+  renderTheUI(toDos);
+}
